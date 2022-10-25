f3f643f1ffc147631e3b2c3f90a93d81142f3c3f
quinn_jh <quinnjarvisholland@gmail.com>
2022-10-24 17:20:39 -0700

    created problem-1.clj
    started problemsolving notes

---
+++ problem-1.clj
@@ -0,0 +1,15 @@
+;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+;; The sum of these multiples is 23.
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+;;
+;; # notes #
+;; List all natural nnumbers below ten that are multiples of three or five
+;; returns 3,5,6 and 9. The sum of the multiples is 23.
+;; For all n<10 ( (if n/3 is integer)==true , print( str( n + " " )))
+;;
+;;
+;;
+;;
+;;
+;;
+;; ( n (if n/3 isInteger) conj(multiples-list)
