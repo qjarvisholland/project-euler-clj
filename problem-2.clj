5e42c6024a785c8fcc4c8651565de0ab07215b44
quinn_jh <quinnjarvisholland@gmail.com>
2022-11-17 00:38:25 -0500

    Update problem-2.clj

--- problem-2.clj
+++ problem-2.clj
@@ -3,3 +3,10 @@
       (= n 0) 0
       (= n 1) 1
         :else (+ (fib (- n 1)) (fib (- n 2)))))
+
+
+(defn get-fib [n] 
+  (loop 
+    [ x [0 1]] 
+    (if (< (count x) n) 
+      (recur (conj x (+ (last x) (nth x (- (count x) 2))))) x)))
