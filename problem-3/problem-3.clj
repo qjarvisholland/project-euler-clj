5ef8181ad21d2272ac8800c4e40378321f442d63
qjarvisholland <quinnjarvisholland@gmail.com>
2022-12-07 14:31:41 -0500

    adding prob 3 solutions

--- problem-3/problem-3.clj
+++ problem-3/problem-3.clj
@@ -18,3 +18,47 @@
 
 ;; (def test-number 600851475143)
 ;; (last (prime-factors(test-number)))
+
+;; (loop [x 0]
+;;	(recur (+1 x)))
+;; 
+
+
+(comment
+
+
+  (loop [x 0]
+    (print x)
+    (recur (+ 1 x)))
+
+  ;; ===
+
+  ((fn [x]
+     (print x)
+     (recur (+ 1 x))) 0)
+
+
+  ;; ===
+  ((fn count-forever [x]
+     (print x)
+     (count-forever (+ 1 x))) 0)
+
+
+  ((fn count-some [x max]
+     (if (< x max)
+       (count-some (+ x 1) max)
+       (do
+         (println x)
+         (println "done!")))) 0 1000000)
+
+
+  ((fn count-some [x max]
+     (if (< x max)
+       (do
+         (println
+           (recur (+ x 1) max)))
+       (do
+         (println x)
+         (println "done!")))) 0 10)
+
+)
