e5a223db9f266cd702f5f472fe86831fcb5d6ffa
qjarvisholland <quinnjarvisholland@gmail.com>
2022-12-12 16:54:28 -0500

    created palindrome checker function

--- problem-4/problem-4.clj
+++ problem-4/problem-4.clj
@@ -5,7 +5,11 @@
 
 
 
-(defn is-palindrome
-      (if ((reverse (str n1))= (seq (str n1)))
+(defn is-palindrome [n1]
+      (if (= (reverse (str n1)) (seq (str n1)))
         (println (str n1))))
 
+
+
+
+(defn is-palindrome [n1] (if (= (reverse (str n1)) (seq (str n1))) (println (str  (str n1) " is a palindrome" ))))
