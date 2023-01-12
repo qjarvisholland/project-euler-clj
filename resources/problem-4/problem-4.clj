19693677477a48066075b4e88700be53ec715188
qjarvisholland <quinnjarvisholland@gmail.com>
2023-01-12 12:59:52 -0500

    some progress on prob 4

--- resources/problem-4/problem-4.clj
+++ resources/problem-4/problem-4.clj
@@ -1,6 +1,20 @@
+;; A palindromic number reads the same both ways. 
+;; The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+;; Find the largest palindrome made from the product of two 3-digit numbers.
+;;
+
 
 (def n1 65432123456)
 (reverse (str n1))
 (seq (str n1))
 
 (defn is-palindrome [n1] (if (= (reverse (str n1)) (seq (str n1))) (println (str  (str n1) " is a palindrome" ))))
+
+;; pseudocode section
+;;
+(is-symmetric (= (seq (str n)) (reverse (str n))) true false)
+(is-palindrome (is-symmetric n) true false)
+(three-digit-nums (and (> n 99) (< n 1000)) true false)
+
+
+
