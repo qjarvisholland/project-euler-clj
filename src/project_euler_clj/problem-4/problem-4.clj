a3fd5b96eae5320c1434731e2e9b30301d01040f
qjarvisholland <quinnjarvisholland@gmail.com>
2023-01-26 16:48:27 -0500

    cleaned up function in prob-4

--- src/project_euler_clj/problem-4/problem-4.clj
+++ src/project_euler_clj/problem-4/problem-4.clj
@@ -1,19 +1,22 @@
 ;; A palindromic number reads the same both ways. 
 ;; The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 ;; Find the largest palindrome made from the product of two 3-digit numbers.
-;;
-
 
 (def n1 65432123456)
 (reverse (str n1))
 (seq (str n1))
-(defn is-palindrome [n1] (if 
-                           (= 
-                            (reverse 
-                             (str n1)) 
-                            (seq (str n1)))(str n1)))
+(defn is-palindrome [n1]
+      (if
+        (= (reverse (str n1))
+           (seq (str n1)))
+        (str n1)))
+
+(defn is-palindrome [n1]
+      (if
+        (= (reverse (str n1))
+           (seq (str n1)))))
+
 ;; pseudocode section
-;;
 ;;(is-symmetric (= (seq (str n)) (reverse (str n))) true false)
 ;;(is-palindrome (is-symmetric n) true false)
 ;;(three-digit-nums (and (> n 99) (< n 1000)) true false) ;just use range
@@ -28,3 +31,9 @@
 ;; pseudocode
 ;; (tuple-products (for each x (for each y (* x y))))
 (map (fn [a b] (* a b)) (range 10) (range 10))
+
+(def digits [1 2 3])
+
+(for [x1 digits
+      x2 digits]
+     (* x1 x2))
