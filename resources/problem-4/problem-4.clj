1edd32a0d221c910c34d5255a0ecc42cf7fb4070
quinn_jh <quinnjarvisholland@gmail.com>
2023-01-26 00:30:57 -0500

    working filter still needs tuple product
    working filter, still need to multiply tuples from the two ranges

--- resources/problem-4/problem-4.clj
+++ resources/problem-4/problem-4.clj
@@ -7,14 +7,17 @@
 (def n1 65432123456)
 (reverse (str n1))
 (seq (str n1))
-
-(defn is-palindrome [n1] (if (= (reverse (str n1)) (seq (str n1))) (println (str  (str n1) " is a palindrome" ))))
-
+(defn is-palindrome [n1] (if 
+                           (= 
+                            (reverse 
+                             (str n1)) 
+                            (seq (str n1)))(str n1)))
 ;; pseudocode section
 ;;
-(is-symmetric (= (seq (str n)) (reverse (str n))) true false)
-(is-palindrome (is-symmetric n) true false)
-(three-digit-nums (and (> n 99) (< n 1000)) true false)
+;;(is-symmetric (= (seq (str n)) (reverse (str n))) true false)
+;;(is-palindrome (is-symmetric n) true false)
+;;(three-digit-nums (and (> n 99) (< n 1000)) true false) ;just use range
 
+(filter is-palindrome (range 99 1000))
 
-
+(last (filter is-palindrome (range 99 1000)))
