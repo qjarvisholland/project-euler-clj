464bf5e15a84109e2c7251334c42325da67135c8
qjarvisholland <quinnjarvisholland@gmail.com>
2023-02-02 14:00:23 -0500

    noodling on problem 5

---
+++ src/project_euler_clj/problem-5/problem-5.clj
@@ -0,0 +1,35 @@
+(defn eligible []
+      (for
+        [n]
+        (if
+          (can-be-divided-by (range 1 20) (min (n))))
+        (print n)
+        (inc n)))                                           ;; doesnt work
+
+
+(def a (atom 10))
+;; #'user/a
+
+(while (pos? @a)
+       (println @a)
+       (swap! a dec))
+
+(def n (atom 1))
+
+(while (whilecondition)
+       (print @n)
+       (swap! n inc))
+
+(def n (atom 1))
+(first
+  (if (zero? (rem (range 1 20) @n))
+         @n
+         (swap! n inc))
+  )
+
+(if (zero? (rem (range 1 20) @z))
+          (swap! z inc)
+          (print @z))
+(while (zero? (rem @z (range 1 20)))
+       (print @z)
+       (swap! z inc))
